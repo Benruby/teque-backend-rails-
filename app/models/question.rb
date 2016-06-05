d7d11a0a00e8e663c76a4eb95a0f84adc0725305
benruby <benlevi1@gmail.com>
2016-06-05 22:10:40 +0300

    adding char counters for question bar

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,10 @@
 class Question < ActiveRecord::Base
 
+	validates :title, length: { minimum: 5 }
+	validates :title, length: { maximum: 150 }
+	validates :body, length: { minimum: 5 }
+	validates :body, length: { maximum: 250 }
+
 	belongs_to :user
 	has_many :answers
 	has_many :question_upvotes
